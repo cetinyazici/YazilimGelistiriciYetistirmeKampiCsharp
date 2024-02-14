48a49a9ed733af5023447b877446dad0e18867b5
cetinyazici <cetin.yazici30@erzurum.edu.tr>
2024-02-14 20:00:37 +0300

    added Loops

---
+++ IkinciGünOdev1/Loops/Program.cs
@@ -0,0 +1,47 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Loops");
+        Console.WriteLine("-----------");
+
+        int number = 7;
+        int number2 = 4;
+
+        //     başlangıç     şart       artış
+        for (int i = 0; i <= number; i = i + 2)
+        {
+            Console.WriteLine(i);
+        }
+
+        Console.WriteLine("----------");
+
+        while (number >= 0)
+        {
+            Console.WriteLine(number);
+            number--;
+        }
+
+        Console.WriteLine("----------");
+
+        //önce döngüye girer sonra şarta ilk etapta şart sağlanmasa bile yazdırma işlemi gerçekleşir.
+        do
+        {
+            Console.WriteLine(number2);
+            number2--;
+
+        } while (number2 >= 0);
+
+        Console.WriteLine("----------");
+
+        string[] students = { "Ahmet", "Çetin", "Ayşe", "Hasan" };
+
+        foreach (var student in students)
+        {
+            Console.WriteLine(student);
+        }
+
+
+
+    }
+}
