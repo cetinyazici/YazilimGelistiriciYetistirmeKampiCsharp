42041ae821d76ed056124f251811966691ad0393
cetinyazici <cetin.yazici30@erzurum.edu.tr>
2024-02-14 03:34:34 +0300

    Updated TypesAndVariables and added Conditionals

---
+++ IkinciGünOdev1/Conditionals/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var number1 = 10;
+int number2 = 10;
+
+Console.WriteLine("Number1: " + number1);
+Console.WriteLine("Number2: " + number2);
+Console.WriteLine("------------------");
+
+if (number1 == 11)
+{
+    Console.WriteLine("Number is 10");
+}
+else if (number1 == 20)
+{
+    Console.WriteLine("Number is 20");
+}
+else
+{
+    Console.WriteLine("Number is not 10 or 20");
+}
+//                  şart kısmı          true            false
+Console.WriteLine(number1 == 10 ? "Number is 10" : "Number is not 10");
+Console.WriteLine("------------------");
+
+switch (number2)
+{
+    case 10:
+        Console.WriteLine("Number is 10");
+        break;
+    case 20:
+        Console.WriteLine("Number is 20");
+        break;
+    default:
+        Console.WriteLine("Number is not 10 or 20");
+        break;
+}
+Console.WriteLine("------------------");
+
+int number3 = 500;
+if (number3>=0 && number3<=100)
+{
+    Console.WriteLine("Number is between 0-100");
+}
+else if (number3>100 && number3<=200)
+{
+    Console.WriteLine("Number is between 100-200");
+}
+else if(number3<0 || number3>200)
+{
+    Console.WriteLine("Number is less than 0 or greater than 200");
+}
+Console.WriteLine("------------------");
+
+
--- IkinciGünOdev1/TypesAndVariables/Program.cs
+++ IkinciGünOdev1/TypesAndVariables/Program.cs
@@ -3,6 +3,33 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World");
 
 //Value Types
+int number1 = 25; //32 bit yer kaplar.
+long number2 = 50; //64 bit yer kaplar. 19 karakterden oluşur
+short number3 = 12; //16 bit yer kaplar.
+byte number4 = 255; //8 bit yer kaplar.
+bool condition = true;
+char character = 'A';
+double number5 = 2.5; //64 bit yer kaplar.
+decimal number6 = 5.0m; //sayıdan sonra m koymamız lazım
+var number7 = 45; //Değere göre tip alır.
+number7 = 'A'; //ascıı karaketerine karşılık geldi.
 
-int number1 = 25;
-Console.Write(number1);
+
+Console.WriteLine("Number1 is " + number1);
+Console.WriteLine("Number2 is " +  number2);
+Console.WriteLine("Number3 is " + number3);
+Console.WriteLine("Number4 is " + number4);
+Console.WriteLine("Bool is " + condition);
+Console.WriteLine("Char is " + character);
+Console.WriteLine("Char is " + (int)character); //ASCII karakter karşılığı.
+Console.WriteLine("Number5 is " + number5);
+Console.WriteLine("Decimal Number6 is " + number6);
+Console.WriteLine(Days.Friday);
+Console.WriteLine((int)Days.Friday);
+Console.Write("Var Number7 is " + number7);
+
+enum Days
+{
+    //0        1           2        3        4         5       6   
+    Monday=1, Tueasday, Wednestay, Thursday, Friday, Saturday, Sandey
+}
