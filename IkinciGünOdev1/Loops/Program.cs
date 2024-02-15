87a2d8295fda45867761efa36b14825d5e33d142
cetinyazici <cetin.yazici30@erzurum.edu.tr>
2024-02-15 15:48:36 +0300

    added IsPrimeNumber  method

--- IkinciGünOdev1/Loops/Program.cs
+++ IkinciGünOdev1/Loops/Program.cs
@@ -41,7 +41,28 @@
             Console.WriteLine(student);
         }
 
+        if (IsPrimeNumber(7))
+        {
+            Console.WriteLine("This is a prime number.");
+        }
+        else
+        {
+            Console.WriteLine("This is not a prime number");
+        }
+    }
 
+    private static bool IsPrimeNumber(int number)
+    {
+        bool result = true;
 
+        for (int i = 2; i < number-1; i++)
+        {
+            if(number%i==0)
+            {
+                result= false;
+                i = number;
+            }
+        }
+        return result;
     }
 }
