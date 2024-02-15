bf003d93810399386d014796a9543d84297835c7
cetinyazici <cetin.yazici30@erzurum.edu.tr>
2024-02-16 02:09:47 +0300

    added Inheritances

---
+++ IkinciGünOdev1/Inheeritance/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Security.Authentication;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Inheritance: kalıtım, miras alma");
+        //interface birden fazla kullanılabilirken inheritance de bir classın birden fazla babası olamaz.
+        //interface-> tek başına bir anlam ifade etmez
+        //inheritance-> tek başına bir anlam ifade eder.
+
+        Person[] persons = new Person[3]
+        {
+            new Person{FirstName="Çetin"},
+            new Customer{FirstName="Ahmet"},
+            new Student{FirstName="Ali"},
+        };
+
+        foreach (var person in persons)
+        {
+            Console.WriteLine(person.FirstName);
+            
+        }
+
+
+        Customer customer = new Customer();
+        customer.Id = 1;
+        customer.FirstName = "Test";
+        customer.LastName = "Test";
+        customer.City = "Test";
+    }
+
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    class Customer : Person
+    {
+        public string City { get; set; }
+    }
+
+    class Student : Person
+    {
+        public string Departmant { get; set; }
+    }
+}
