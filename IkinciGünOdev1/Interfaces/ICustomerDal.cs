53fb31747c5c30b42d519e0233e3f974ef0f8685
cetinyazici <cetin.yazici30@erzurum.edu.tr>
2024-02-16 00:15:50 +0300

    added Interfaces

---
+++ IkinciGünOdev1/Interfaces/ICustomerDal.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    interface ICustomerDal //Dal:DataAcces->Veri Erişimleri
+    {
+        void Add();
+        void Update();
+        void Delete();
+    }
+
+    class SqlServerCustomerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Sql Added!");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Sql Deleted!");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Sql Updated!");
+        }
+    }
+
+    class OracleCustomerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Oracle Added!");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Oracle Deleted!");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Oracle Updated!");
+        }
+    }
+
+    class MySglCustomerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("MySgl Added!");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("MySgl Deleted!");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("MySgl Updated!");
+        }
+    }
+    class CustomerManager
+    {
+        public void Add(ICustomerDal customerDal)
+        {
+            customerDal.Add();
+        }
+    }
+
+
+}
---
+++ IkinciGünOdev1/Interfaces/Program.cs
@@ -0,0 +1,100 @@
+﻿using Interfaces;
+using System.ComponentModel;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("İNTERFACES");
+
+        //InterfaceIntro();
+        //Demo();
+
+        //İnterfaceler asla newlenemezler
+        //katmanlar arası geçişlerde fazlasıyla interface kullanılır bağımlılığı azaltmak için.
+
+        ICustomerDal[] customerDals = new ICustomerDal[3]
+        {
+            new SqlServerCustomerDal(),
+            new OracleCustomerDal(),
+            new MySglCustomerDal()
+        };
+
+        foreach (var customerDal in customerDals)
+        {
+            customerDal.Add();
+        }
+
+
+    }
+
+    private static void Demo()
+    {
+        CustomerManager customerManager = new CustomerManager();
+        customerManager.Add(new SqlServerCustomerDal());
+        customerManager.Add(new OracleCustomerDal());
+    }
+
+    private static void InterfaceIntro()
+    {
+        Customer customer1 = new Customer();
+        customer1.Id = 1;
+        customer1.FirstName = "Çetin";
+        customer1.LastName = "Yazıcı";
+        customer1.Address = "Erzurum";
+
+        Student student1 = new Student();
+        student1.Id = 2;
+        student1.FirstName = "Ali";
+        student1.LastName = "Akın";
+        student1.Departmant = "Computer Sciences";
+
+        Morker morker1 = new Morker();
+
+        PersonManager personManager = new PersonManager();
+        personManager.Add(customer1);
+        personManager.Add(student1);
+        personManager.Add(morker1);
+    }
+}
+
+//soyut
+interface IPerson
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+}
+
+//somut
+class Customer : IPerson
+{
+
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Address { get; set; }
+}
+class Student : IPerson
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Departmant { get; set; }
+}
+
+class Morker : IPerson
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Departmant { get; set; }
+}
+class PersonManager
+{
+    public void Add(IPerson person)
+    {
+        Console.WriteLine("Added Customer1 " + person.FirstName);
+    }
+}
