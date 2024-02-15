3829741c9bb32e79b9140447c22a49a2e8908eaf
cetinyazici <cetin.yazici30@erzurum.edu.tr>
2024-02-16 00:46:33 +0300

    added InterfacesDemo

---
+++ IkinciGünOdev1/InterfacesDemo/Program.cs
@@ -0,0 +1,84 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("INTERFACE DEMO");
+
+        IWorker[] workers = new IWorker[3]
+        {
+            new Worker(),
+            new Manager(),
+            new Robot(),
+        };
+
+        foreach (var worker in workers)
+        {
+            worker.Work();
+        }
+
+        IEat[] eats = new IEat[2]
+        {
+            new Worker(),
+            new Manager(),
+        };
+
+    }
+
+    interface IWorker
+    {
+        void Work();
+    }
+    interface IEat
+    {
+        void Eat();
+    }
+    interface ISalary
+    {
+        void Salary();
+    }
+
+    class Manager : IWorker, IEat, ISalary
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Ate!!");
+        }
+
+        public void Salary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Worker : IWorker, IEat, ISalary
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Ate!!");
+        }
+
+        public void Salary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Robot : IWorker
+    {
+        public void Work()
+        {
+            Console.WriteLine("");
+        }
+    }
+
+}
